09816080229a7a59ffba40685d048fa676820655
Yunus Emre Ertürk <yemrerturk@gmail.com>
2019-03-21 14:29:26 +0300

    first commit

---
+++ Mart14/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Mart14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbConnected = new System.Windows.Forms.ListBox();
+            this.lbDisconnected = new System.Windows.Forms.ListBox();
+            this.lbEntity = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnConnected = new System.Windows.Forms.Button();
+            this.btnDisconnected = new System.Windows.Forms.Button();
+            this.btnEntity = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbConnected
+            // 
+            this.lbConnected.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbConnected.FormattingEnabled = true;
+            this.lbConnected.ItemHeight = 20;
+            this.lbConnected.Location = new System.Drawing.Point(12, 32);
+            this.lbConnected.Name = "lbConnected";
+            this.lbConnected.Size = new System.Drawing.Size(134, 284);
+            this.lbConnected.TabIndex = 0;
+            // 
+            // lbDisconnected
+            // 
+            this.lbDisconnected.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbDisconnected.FormattingEnabled = true;
+            this.lbDisconnected.ItemHeight = 20;
+            this.lbDisconnected.Location = new System.Drawing.Point(178, 32);
+            this.lbDisconnected.Name = "lbDisconnected";
+            this.lbDisconnected.Size = new System.Drawing.Size(134, 284);
+            this.lbDisconnected.TabIndex = 1;
+            // 
+            // lbEntity
+            // 
+            this.lbEntity.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbEntity.FormattingEnabled = true;
+            this.lbEntity.ItemHeight = 20;
+            this.lbEntity.Location = new System.Drawing.Point(344, 32);
+            this.lbEntity.Name = "lbEntity";
+            this.lbEntity.Size = new System.Drawing.Size(134, 284);
+            this.lbEntity.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Connected";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(178, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Disconnected";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(344, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Entity";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(9, 342);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Connected";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(178, 342);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Disconnected";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(344, 342);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 20);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Entity";
+            // 
+            // btnConnected
+            // 
+            this.btnConnected.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnConnected.Location = new System.Drawing.Point(12, 368);
+            this.btnConnected.Name = "btnConnected";
+            this.btnConnected.Size = new System.Drawing.Size(75, 36);
+            this.btnConnected.TabIndex = 9;
+            this.btnConnected.Text = "C get Data";
+            this.btnConnected.UseVisualStyleBackColor = true;
+            this.btnConnected.Click += new System.EventHandler(this.btnConnected_Click);
+            // 
+            // btnDisconnected
+            // 
+            this.btnDisconnected.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDisconnected.Location = new System.Drawing.Point(178, 368);
+            this.btnDisconnected.Name = "btnDisconnected";
+            this.btnDisconnected.Size = new System.Drawing.Size(75, 36);
+            this.btnDisconnected.TabIndex = 10;
+            this.btnDisconnected.Text = "D get Data";
+            this.btnDisconnected.UseVisualStyleBackColor = true;
+            this.btnDisconnected.Click += new System.EventHandler(this.btnDisconnected_Click);
+            // 
+            // btnEntity
+            // 
+            this.btnEntity.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEntity.Location = new System.Drawing.Point(344, 368);
+            this.btnEntity.Name = "btnEntity";
+            this.btnEntity.Size = new System.Drawing.Size(75, 36);
+            this.btnEntity.TabIndex = 11;
+            this.btnEntity.Text = "E get data";
+            this.btnEntity.UseVisualStyleBackColor = true;
+            this.btnEntity.Click += new System.EventHandler(this.btnEntity_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 405);
+            this.Controls.Add(this.btnEntity);
+            this.Controls.Add(this.btnDisconnected);
+            this.Controls.Add(this.btnConnected);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbEntity);
+            this.Controls.Add(this.lbDisconnected);
+            this.Controls.Add(this.lbConnected);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbConnected;
+        private System.Windows.Forms.ListBox lbDisconnected;
+        private System.Windows.Forms.ListBox lbEntity;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnConnected;
+        private System.Windows.Forms.Button btnDisconnected;
+        private System.Windows.Forms.Button btnEntity;
+    }
+}
+
---
+++ Mart14/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mart14
+{
+    //@Author Yunus Emre Ertürk=>>> yemrerturk@gmail.com / www.muhendiserturk.com
+    public partial class Form1 : Form
+    {
+        bool a=true;
+        DateTime baslangic = new DateTime();
+        Stopwatch stopwatch = new Stopwatch();
+        double zaman = 0;
+        SqlConnection Conn = new SqlConnection(
+            ConfigurationManager.ConnectionStrings["NorthwindConnectionString"].
+            ConnectionString);
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval=10000;
+        }
+
+        private void btnEntity_Click(object sender, EventArgs e)
+        {
+            stopwatch.Restart();
+            NorthwindEntities db = new NorthwindEntities();            
+            
+            if (a)
+            {
+                lbEntity.DataSource = db.Orders.Select(x => x.OrderID).ToList();
+                a = false;
+                label3.Text = "ORders";
+            }
+            else
+            {
+                label3.Text = "ORder Details";
+                a = true;
+                lbEntity.DataSource = db.Order_Details.Select(x => x.OrderID).ToList();
+            }
+
+
+            stopwatch.Stop();
+            label6.Text = stopwatch.ElapsedMilliseconds.ToString();
+            
+            
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            zaman++;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 1;
+            timer1.Enabled=true;
+            baslangic = DateTime.Now;
+            
+        }
+
+        private void btnDisconnected_Click(object sender, EventArgs e)
+        {
+            stopwatch.Restart();
+            //timer1.Start();          
+            SqlCommand cmd = new SqlCommand("Select OrderID,CustomerID from Orders",Conn);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sqlDataAdapter.Fill(dt);
+            foreach (DataRow item in dt.Rows)
+            {
+                lbDisconnected.Items.Add(item[0]+" "+ item[1]);     
+                
+            }
+            //label5.Text = zaman.ToString();
+            //timer1.Stop();
+            //zaman = 0;
+            stopwatch.Stop();
+            label5.Text = stopwatch.ElapsedMilliseconds.ToString();
+
+
+        }
+
+        private void btnConnected_Click(object sender, EventArgs e)
+        {
+            
+            stopwatch.Restart();
+            //timer1.Start();
+            SqlCommand cmd = new SqlCommand("Select OrderID from Orders",Conn);
+            Conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    lbConnected.Items.Add(reader.GetInt32(0));
+                }
+            }
+            else
+            {
+                Console.WriteLine("No rows found.");
+            }
+            reader.Close();
+            Conn.Close();
+            //label4.Text = zaman.ToString();
+            //timer1.Stop();
+            //zaman = 0;
+            stopwatch.Stop();
+            label4.Text = stopwatch.ElapsedMilliseconds.ToString();
+
+        }
+    }
+}
+//@Author Yunus Emre Ertürk=>>> yemrerturk@gmail.com / www.muhendiserturk.com
